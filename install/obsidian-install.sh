e6ac0693b7b9c92958e4a9a8845593b8e6846dba
Sixto Cantolla Sanchez <sixto.cantolla@gmail.com>
2025-05-30 16:57:13 +0100

    chore: Dynamically fetch latest Obsidian version

--- install/obsidian-install.sh
+++ install/obsidian-install.sh
@@ -27,7 +27,14 @@ $STD apt-get install -y \
   ssh-askpass
 msg_ok "Installed Dependencies"
 
-OBSIDIAN_VERSION="1.7.7"
+msg_info "Fetching latest Obsidian version"
+OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+if [ -z "$OBSIDIAN_VERSION" ]; then
+  msg_error "Failed to fetch the latest Obsidian version. Exiting."
+  exit 1
+fi
+msg_ok "Fetched latest Obsidian version: v${OBSIDIAN_VERSION}"
+
 msg_info "Installing Obsidian v${OBSIDIAN_VERSION}"
 $STD curl --location --output obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
 $STD dpkg -i obsidian.deb
